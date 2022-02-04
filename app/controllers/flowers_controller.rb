a45d5649a11c3d708074e9888bea2c87d95e5ed5
Amulya <amulyaanmolg@gmail.com>
2022-02-04 13:55:45 +0530

    Controller CRUD actions implemented

--- app/controllers/flowers_controller.rb
+++ app/controllers/flowers_controller.rb
@@ -1,9 +1,41 @@
 class FlowersController < ApplicationController
-    def index
-      @flowers = Flower.all
-    end
+  def index
+    @flowers = Flower.all
+  end
 
-    def show
-      @flower = Flower.find(params[:id])
+  def show
+    @flower = Flower.find(params[:id])
+  end
+
+  def new
+    @flower = Flower.new
+  end
+
+  def create
+    @flower = Flower.new(params.require(:flower).permit(:name, :price))
+    if @flower.save
+      redirect_to @flower
+    else
+      render :new
     end
+  end
+
+  def edit
+    @flower = Flower.find(params[:id])
+  end
+
+  def update
+    @flower = Flower.find(params[:id])
+    if @flower.update(params.require(:flower).permit(:name, :price))
+      redirect_to @flower
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @flower = Flower.find(params[:id])
+    @flower.destroy
+    redirect_to root_path
+  end
 end
